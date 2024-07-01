f2b25dc705a9f4a4d1f0feaebafec952ebbd3195
周海 <2358853434@qq.com>
2024-07-01 10:40:40 +0800

    若依admin文件夹合并

--- ruoyi-admin/RuoYi-Vue3-master/src/layout/components/Navbar.vue
+++ ruoyi-admin/RuoYi-Vue3-master/src/layout/components/Navbar.vue
@@ -7,14 +7,14 @@
     <div class="right-menu">
       <template v-if="appStore.device !== 'mobile'">
         <header-search id="header-search" class="right-menu-item" />
-
+<!-- 
         <el-tooltip content="源码地址" effect="dark" placement="bottom">
           <ruo-yi-git id="ruoyi-git" class="right-menu-item hover-effect" />
         </el-tooltip>
 
         <el-tooltip content="文档地址" effect="dark" placement="bottom">
           <ruo-yi-doc id="ruoyi-doc" class="right-menu-item hover-effect" />
-        </el-tooltip>
+        </el-tooltip> -->
 
         <screenfull id="screenfull" class="right-menu-item hover-effect" />
 
